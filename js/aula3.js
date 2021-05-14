d265ca430d75c9a5c773e7cd2f1f4cf50c16df75
JoaaoPaulo <joaocezar_social@hotmail.com>
2021-05-14 17:07:50 -0300

    Atualizacao das aulas 1, 2 e 3

---
+++ js/aula3.js
@@ -0,0 +1,42 @@
+var d = new Date();
+
+alert(d);
+//alert(d.getMonth()+1);
+//alert(d.getMinutes());
+//alert(d.getday);
+//alert(d.getHours);
+
+
+
+
+
+
+
+/*
+Estrutura de Repeticao
+
+var count;
+for(count = 0; count <= 5; count++){
+    alert(count);
+}
+
+==================
+
+var count = 1;
+while(count <= 5){
+    console.log(count);
+    alert(count)
+    count++;
+}
+*/
+
+
+/*
+Estrutura condicional 
+
+var idade = prompt("Qual a sua Idade: ")
+if(idade >= 18){
+    alert("Maior de Idade");
+}else{
+    alert("Menor de idade");
+}*/
