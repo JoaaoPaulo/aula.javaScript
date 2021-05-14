694e9ead8c2731a35cf058d6262075d60f2b76ab
JoaaoPaulo <joaocezar_social@hotmail.com>
2021-05-14 16:03:19 -0300

    Prmeiros comandos em JavaScript

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// alert("Meu primeiro js"); - Alerta simples
+
+/*
+var idade = 22;
+var nome = "Joao Paulo";
+alert(nome + " tem " + idade + " anos"); -alerta com variavel
+
+===========================
+
+var idade = 22;
+var idade1 = 10;
+alert(idade + idade1); - alerta com operacao aritmetica
+
+===========================
+
+Mensagem no Console do NAVEGADOR
+
+ar idade = 22;
+var nome = "Joao Paulo";
+
+console.log(nome);
+console.log(idade);
+
+
+var frase = "japao e o melhor time do mundo";
+
+console.log(frase.replace("japao", "brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase()); 
+
+*/ 
